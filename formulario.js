61196535c0de3a756358dbb6f84d7d8d95442a65
Yeny <yenyleydygarcia@gmail.com>
2023-04-09 21:01:23 +0200

    come

---
+++ formulario.js
@@ -0,0 +1,15 @@
+function validar (){
+    var nombre, apellido, email, password; 
+    email= document.gemailetElementById("email").value
+    password = document.getElementById("password").value}
+
+if(email ===""){
+"¿Has olvidado tu email?"
+return false;
+    }
+ if(password ===""){
+"¿Has olvidado tu password?"
+ return false;
+} 
+
+else if
---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("miBoton").onclick = function () {
+
+    let usuario = document.getElementById("miTexto").value;
+
+    document.getElementById("miLabel").innerHTML = "Hola YENY " + usuario;
+}
